04fb2c863a51afa01cdb2db91643bd5def4b5e7e
Eugene Lazutkin <eugene@lazutkin.com>
2012-05-29 00:12:41 -0500

    Renamed an internal variable of Cleanup.

--- mixins/Cleanup.js
+++ mixins/Cleanup.js
@@ -12,32 +12,32 @@
 	return dcl(Destroyable, {
 		//declaredClass: "dcl/mixins/Cleanup",
 		constructor: function(){
-			this._cleanup_stack = [];
+			this.__cleanupStack = [];
 		},
 		pushCleanup: function(resource, cleanup){
 			var f = cleanup ? function(){ cleanup(resource); } : function(){ resource.destroy(); };
-			this._cleanup_stack.push(f);
+			this.__cleanupStack.push(f);
 			return f;
 		},
 		popCleanup: function(dontRun){
 			if(dontRun){
-				return this._cleanup_stack.pop();
+				return this.__cleanupStack.pop();
 			}
-			this._cleanup_stack.pop()();
+			this.__cleanupStack.pop()();
 		},
 		removeCleanup: function(f){
-			for(var i = this._cleanup_stack.length - 1; i >= 0; --i){
-				if(this._cleanup_stack[i] === f){
-					this._cleanup_stack.splice(i, 1);
+			for(var i = this.__cleanupStack.length - 1; i >= 0; --i){
+				if(this.__cleanupStack[i] === f){
+					this.__cleanupStack.splice(i, 1);
 					return true;
 				}
 			}
 		},
 		cleanup: function(){
-			for(var i = this._cleanup_stack.length - 1; i >= 0; --i){
-				this._cleanup_stack[i]();
+			for(var i = this.__cleanupStack.length - 1; i >= 0; --i){
+				this.__cleanupStack[i]();
 			}
-			this._cleanup_stack = [];
+			this.__cleanupStack = [];
 		},
 		destroy: function(){
 			this.cleanup();
