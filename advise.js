df7663a39f806787fea963e8af84dbfe82e074e5
Eugene Lazutkin <eugene@lazutkin.com>
2012-07-15 13:15:10 -0500

    Minor refactoring, added support for an error processing of around advices.

--- advise.js
+++ advise.js
@@ -9,53 +9,55 @@
 })(function(){
 	"use strict";
 
-	var Node = function(){
-			this.nb = this.pb = this.na = this.pa = this.nf = this.pf = this;
+	function Node(instance, name){
+		this.nb = this.pb = this.na = this.pa = this.nf = this.pf = this;
+		this.i = instance;
+		this.n = name;
+	}
+	var p = Node.prototype = {
+		a: function(b, a, f, o){
+			var t = new Node(this.i, this.n);
+			t.p = this;
+			t.b = b;
+			this._a("b", t);
+			t.a = a;
+			this._a("a", t);
+			t.f = f;
+			this._a("f", t, o);
+			t.o = o;
+			if(o){ t.f = advise._f(o, t.pf.f, this); }
+			return t;
 		},
-		p = {
-			a: function(b, a, f, o){
-				var t = new Node;
-				t.p = this;
-				t.b = b;
-				this._a("b", t);
-				t.a = a;
-				this._a("a", t);
-				t.f = f;
-				this._a("f", t, o);
-				t.o = o;
-				if(o){ t.f = o(t.pf.f); }
-				return t;
-			},
-			_a: function(topic, node, flag){
-				if(node[topic] || flag){
-					var n = "n" + topic, p = "p" + topic;
-					(node[p] = this[p])[n] = (node[n] = this)[p] = node;
-				}
-			},
-			r: function(node){
-				this._r("b", node);
-				this._r("a", node);
-				this._r("f", node);
-			},
-			_r: function(topic, node){
+		_a: function(topic, node, flag){
+			if(node[topic] || flag){
 				var n = "n" + topic, p = "p" + topic;
-				node[n][p] = node[p];
-				node[p][n] = node[n];
-			},
-			destroy: function(){
-				var f = this.pf.f, t = this.nf, p = this.p;
-				this.r(this);
-				if(t !== this){
-					for(; t !== p; f = t.f, t = t.nf){
-						if(t.o){
-							t.f = t.o(f);
-						}
+				(node[p] = this[p])[n] = (node[n] = this)[p] = node;
+			}
+		},
+		r: function(node){
+			this._r("b", node);
+			this._r("a", node);
+			this._r("f", node);
+		},
+		_r: function(topic, node){
+			var n = "n" + topic, p = "p" + topic;
+			node[n][p] = node[p];
+			node[p][n] = node[n];
+		},
+		destroy: function(){
+			var f = this.pf.f, t = this.nf, p = this.p;
+			this.r(this);
+			if(t !== this){
+				for(; t !== p; f = t.f, t = t.nf){
+					if(t.o){
+						t.f = advise._f(t.o, f, this);
 					}
 				}
 			}
-		};
+			this.i = 0;
+		}
+	};
 	p.unadvise = p.destroy;   // alias
-	Node.prototype = p;
 
 	function makeAOPStub(x){
 		var f = function(){
@@ -88,7 +90,7 @@
 		if(f && f.adviceNode && f.adviceNode instanceof Node){
 			a = f.adviceNode;
 		}else{
-			a = new Node;
+			a = new Node(instance, name);
 			if(f && f.advices){
 				f = f.advices;
 				a.a(f.b, f.a, f.f);
@@ -97,13 +99,16 @@
 			}
 			instance[name] = makeAOPStub(a);
 		}
-		if(advice instanceof Function){ advice = advice(name, instance); }
+		if(typeof advice == "function"){ advice = advice(name, instance); }
 		return a.a(advice.before, advice.after, 0, advice.around);
 	}
 
 	advise.before = function(instance, name, f){ return advise(instance, name, {before: f}); };
 	advise.after  = function(instance, name, f){ return advise(instance, name, {after: f}); };
 	advise.around = function(instance, name, f){ return advise(instance, name, {around: f}); };
+	advise.Node = Node;
+
+	advise._f = function(f, a, node){ return f(a); };
 
 	return advise;
 });
