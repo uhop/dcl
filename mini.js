185a9967d84ee2fbcb841f1296d143d0ec317cc5
Eugene Lazutkin <eugene@lazutkin.com>
2013-05-24 14:42:34 -0500

    Bugfix: special treatment for non-dcl constructor functions.

--- mini.js
+++ mini.js
@@ -140,7 +140,7 @@
 			var i = bases.length - 1, r = [], b, f, around = advice == "f";
 			for(; b = bases[i]; --i){
 				// next line contains 5 intentional assignments
-				if((f = b._m) ? (f = f.h).hasOwnProperty(name) && (isSuper(f = f[name]) ? (around ? f.f : (f = f[advice])) : around) : around && (f = b[pname][name]) && f !== empty[name]){
+				if((f = b._m) ? (f = f.h).hasOwnProperty(name) && (isSuper(f = f[name]) ? (around ? f.f : (f = f[advice])) : around) : around && (f = name == cname ? b : b[pname][name]) && f !== empty[name]){
 					f.ctr = b;
 					r.push(f);
 				}
