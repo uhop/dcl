026f861cc3359bcf283136f92391232826acd391
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2017-06-09 21:07:30 -0500

    Site updated at 2017-06-10 02:07:08 UTC

---
+++ 2.x/downloads/code/cheatsheet.js
@@ -0,0 +1,18 @@
+// *Version 2.x*
+
+// *This file is written using a literate JavaScript. You can download
+// [this cheatsheet](/2.x/downloads/code/cheatsheet.js) to use as a local reference
+// or to run it locally.*
+
+// *If you prefer to see text as inline comments, just click on a sidebar
+// handle at the far right.*
+
+// *While `dcl` works great in browsers using an AMD loader or
+// even simple `<script>`, this tutorial is assumed to be run with
+// [node.js](http://nodejs.org).*
+
+// For our examples we will need the main
+// [dcl](./dcl_js) module:
+var dcl = require("dcl");
+
+// TBD
---
+++ 2.x/downloads/code/tutorial.js
@@ -0,0 +1,17 @@
+// *Version 2.x*
+
+// *This file is written using a literate JavaScript. You can download
+// [this tutorial](/2.x/downloads/code/tutorial.js) to use as a local reference
+// or to run it locally.*
+
+// *If you prefer to see text as inline comments, just click on a sidebar
+// handle at the far right.*
+
+// *While `dcl` works great in browsers using an AMD loader or
+// even simple `<script>`, this tutorial is assumed to be run with
+// [node.js](http://nodejs.org).*
+
+// For our examples we will need the main [dcl](./dcl_js) module:
+var dcl = require("dcl");
+
+// TBD
